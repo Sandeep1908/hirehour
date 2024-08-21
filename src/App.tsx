6992679b98e66f8ef3310465411a743480e4b67d
Sandeep <sandbro7163337@gmail.com>
2024-08-21 17:05:30 +0530

    feat(add): import compnents

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import Header from './components/Header';
 import Home from './pages/home/Home';
+import Footer from './components/Footer';
+import Signin from './pages/Signin/Signin';
+import Signup from './pages/Signup/Signup';
+import JobDescription from './pages/JobDescription/JobDescription';
 
 const App: React.FC = () => {
   return (
     <>
       <Header />
       <Home/>
+      <Signin/>
+      <Signup/>
+      <JobDescription/>
+      <Footer/>
     </>
   );
 };
