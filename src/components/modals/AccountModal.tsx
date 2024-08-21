5bfc4f9bd4b0ca1fbeabf931cb2ba5aa95171123
Sandeep <sandbro7163337@gmail.com>
2024-08-21 17:06:46 +0530

    feat(modals): add accountModal and Notificationmodals

---
+++ src/components/modals/AccountModal.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import { CgProfile } from "react-icons/cg";
+import { RiListSettingsLine } from "react-icons/ri";
+import { RiUserSettingsLine } from "react-icons/ri";
+import { VscNotebook } from "react-icons/vsc";
+import { GoSignOut } from "react-icons/go";
+
+
+
+type AccountModalProps={
+    isAccountOpen:boolean,
+}
+
+const AccountModal: React.FC<AccountModalProps> = ({isAccountOpen}) => {
+    const accountOptions=[
+        {
+            icon: <CgProfile />,
+            name:'Profile'
+        },
+        {
+            icon:<RiListSettingsLine />,
+            name:'Job Preference'
+        },
+
+        {
+            icon:<RiUserSettingsLine />,
+            name:'Account Setting'
+        },
+        {
+            icon:<VscNotebook />,
+            name:'Our Blog'
+        },
+
+    ]
+  return (
+    <div
+      className={`w-40 h-56 overflow-auto p-3 bg-white absolute  top-16 left-0  rounded-lg transition-all duration-500 shadow-2xl ${isAccountOpen ? 'opacity-1 translate-y-[-10px]' : 'opacity-0   z-[-10]'}`}
+    >
+       
+
+ 
+        <div className="flex flex-col justify-between items-center">
+          <div className="flex justify-start items-center space-x-3 relative before:absolute before:w-full before:bottom-[-10px] before:bg-[#E3E7EA] before:h-[1px] ">
+            <p className="text-xs text-[#104B53] w-6 h-6 flex justify-center items-center bg-[#CBFFFC] rounded-full ">
+              J
+            </p>
+
+            <div className="flex flex-col">
+              <h1 className="text-xs">Johnson</h1>
+              <p className="text-[7px]">johnson@example.com </p>
+             
+            </div>
+
+
+          </div>
+
+          <ul className='flex flex-col space-y-2 pt-5 relative  before:absolute before:w-full before:bottom-[-10px] before:bg-[#E3E7EA] before:h-[1px]'>
+            {accountOptions?.map((item,id)=>{
+                return(
+                    <li className='flex justify-start items-center space-x-2 p-1' key={id}>
+                        {item.icon}
+                        <p className='text-[10px] '>{item.name}</p>
+                    </li>
+                )
+            })}
+
+            <hr />
+
+<li className='flex justify-start items-center space-x-2 p-1'>
+                       <GoSignOut color='#FF4444'/>
+                        <p className='text-[10px] text-[#FF4444] '>Sign Out</p>
+                    </li>
+          </ul>
+    
+       
+        
+   
+        </div>
+ 
+    
+    </div>
+  );
+};
+
+export default AccountModal;
---
+++ src/components/modals/NotificationModal.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { RxCross2 } from 'react-icons/rx';
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+
+type NotificationModalProps = {
+  isNotification: boolean;
+  setIsNotification: (e: boolean) => void;
+};
+
+const NotificationModal: React.FC<NotificationModalProps> = ({
+  isNotification,
+  setIsNotification,
+}) => {
+  return (
+    <div
+      className={`w-64 max-h-64 overflow-auto p-4 bg-white absolute  top-14 right-0 rounded-lg transition-all duration-500 shadow-2xl ${isNotification ? 'opacity-1 translate-y-[-10px]' : 'opacity-0  z-[-10]'}`}
+    >
+      <div className="relative w-full flex justify-between p-2 items-center before:absolute before:w-full before:bottom-0 before:bg-[#E3E7EA] before:h-[1px]">
+        <h1 className="font-semibold">Notifications</h1>
+        <RxCross2 onClick={() => setIsNotification(false)} />
+      </div>
+
+      <div className="flex flex-col pt-5 space-y-4">
+        <div className="flex justify-between items-center">
+          <div className="flex justify-start items-center space-x-3">
+            <p className="text-md text-[#104B53] w-10 h-10 flex justify-center items-center bg-[#CBFFFC] rounded-full ">
+              M
+            </p>
+
+            <div className="flex flex-col">
+              <h1 className="text-sm">Mathew</h1>
+              <p className="text-[10px]">Lorem Ipsum is simply </p>
+              <p className="text-[10px]">0 min</p>
+            </div>
+          </div>
+
+          <MdOutlineKeyboardArrowRight />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotificationModal;
