70a40eeaaa12dfd02bec045e46d03f2568140a8c
Aditya <aadimalviyaa@gmail.com>
2024-08-21 16:55:10 +0530

    feat(component): add SearchFilter component

---
+++ src/components/SearchFilter.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import arrow_down from '../assets/arrow-down.svg'
+
+
+const SearchFilter: React.FC = () => {
+    return (
+        <div className='flex justify-center items-center gap-2 px-4 py-2 border border-[#114B53] rounded-full'>
+            <p className='text-[14px] font-normal text-[#114B53]'>Remote</p>
+            <img src={arrow_down} alt="" />
+        </div>
+    )
+}
+
+export default SearchFilter
