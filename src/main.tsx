5ba3a97e2087d206c742d0d7e59b424329bc9dc0
Sandeep <sandbro7163337@gmail.com>
2024-08-23 18:57:31 +0530

    feat(add): implement router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+import Home from "./pages/home/Home.tsx";
+import JobDescription from "./pages/JobDescription/JobDescription.tsx";
+import Signup from "./pages/Signup/Signup.tsx";
+import Signin from "./pages/Signin/Signin.tsx";
+import ProfileSetting from "./pages/ProfileSetting/ProfileSetting.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/searchjob" element={<JobDescription />} />
+      <Route path='/account' element={<ProfileSetting/>} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+    </Route>
+  ),
+);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
-)
+);
