42f89ddec02fce4dd4bb2618adaccf51c006a115
Sandeep <sandbro7163337@gmail.com>
2024-08-26 12:35:32 +0530

    feat(main.tsx): add router and pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,36 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 import {
   RouterProvider,
   createBrowserRouter,
   createRoutesFromElements,
   Route,
-} from "react-router-dom";
-import Home from "./pages/home/Home.tsx";
-import JobDescription from "./pages/JobDescription/JobDescription.tsx";
-import Signup from "./pages/Signup/Signup.tsx";
-import Signin from "./pages/Signin/Signin.tsx";
-import ProfileSetting from "./pages/ProfileSetting/ProfileSetting.tsx";
+} from 'react-router-dom';
+import Home from './pages/home/Home.tsx';
+import JobDescription from './pages/JobDescription/JobDescription.tsx';
+import Signup from './pages/Signup/Signup.tsx';
+import Signin from './pages/Signin/Signin.tsx';
+import AdditionalInfo from './pages/ProfileSetting/AdditionalInformation/AdditionalInfo.tsx';
+import UploadResume from './pages/ProfileSetting/UploadResume/UploadResume.tsx';
+import ReviewInfo from './pages/ProfileSetting/ReviewInfo/ReviewInfo.tsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="/" element={<Home />} />
       <Route path="/searchjob" element={<JobDescription />} />
-      <Route path='/account' element={<ProfileSetting/>} />
+      <Route path="/upload-resume" element={<UploadResume />} />
+      <Route path="/additional-information" element={<AdditionalInfo />} />
+      <Route path="/review-form" element={<ReviewInfo />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/signin" element={<Signin />} />
-    </Route>
+    </Route>,
   ),
 );
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
