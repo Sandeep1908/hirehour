0c17a1faca85d35fc06339c281a57a2c0440c232
Sandeep <sandbro7163337@gmail.com>
2024-08-26 12:34:34 +0530

    feat(add): add reivewInfo.ts file

---
+++ src/config/reviewinfo.ts
@@ -0,0 +1,26 @@
+export const reviewInfo = [
+  {
+    label: 'First name',
+    value: 'John',
+  },
+  {
+    label: 'Last name',
+    value: 'Mathew',
+  },
+  {
+    label: 'Email',
+    value: 'John@example.com',
+  },
+  {
+    label: 'Phone number',
+    value: '+1 xxxxxxx909',
+  },
+  {
+    label: 'Visa Sponsership',
+    value: 'Yes',
+  },
+  {
+    label: 'Location',
+    value: 'Texas, united States',
+  },
+];
