c31d72e18809b5281bd05ea41cabbe3a4bb36c43
Sandeep <sandbro7163337@gmail.com>
2024-08-26 12:32:18 +0530

    feat(index.d.ts): add types for reviewDetials

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,5 +1,14 @@
-interface HomeCardTypes{
-    imgUrl:string,
-    title:string,
-    description:string
+//Home card details
+
+interface HomeCardTypes {
+  imgUrl: string;
+  title: string;
+  description: string;
+}
+
+//Review info person details
+
+interface reviewDetailsTypes {
+  label: string;
+  value: string;
 }
