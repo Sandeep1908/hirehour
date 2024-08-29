cf5287305dd9d95c09962f72a900bd8a9ac019f0
Sandeep <sandbro7163337@gmail.com>
2024-08-29 14:53:25 +0530

    feat(pages): add accountsetting page

---
+++ src/pages/AccountSettings/AccountSettings.tsx
@@ -0,0 +1,126 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const AccountSetting:React.FC=()=>{
+    const titles = [
+        {
+            label:'Profile Settings',
+            link:'/myjobs'
+        },
+        {
+            label:'Profile Visibility',
+            link:'/right-to-represent'
+        },{
+            label:'Privacy Settings',
+            link:'/messages'
+        }
+      ];
+    
+    return(
+        <div className="w-full  h-full bg-[#F2F2F5] p-2 " >
+            <div className=" w-full max-w-[1280px] h-full bg-white  m-auto mt-10 rounded-lg p-5">
+                    <h1 className="text-3xl p-3 font-bold">Account Settings</h1>
+                    <hr />
+                        <div className="w-full">
+                                <ul className="w-full  flex justify-start items-center space-x-6   ">
+                                    {titles?.map((item,id)=>{
+                                        return(
+                                            <Link to={item.link} className={` text-sm p-1 md:text-xl font-[600]  ${id==2?'border-[#104B53] border-b-4':''}  md:p-5`} key={id}>{item.label}</Link>
+                                        )
+                                    })}
+                                </ul>
+                        </div>
+
+
+            
+
+                                   
+
+{/* Setting Box  */}
+
+                        <div className="w-full   max-w-[1200px] mt-10  h-full m-auto border border-[#E1E1E2] rounded-lg">
+
+                                        <div className="w-full h-full flex flex-col space-y-10 p-10">
+
+                                                    <div className="flex flex-col space-y-3 relative   before:absolute before:bottom-[-10px] before:w-full before:h-[1px] before:bg-[#ECECEC]">
+                                                            <p className="text-xs text-[#717171]">Full name</p>
+                                                            <p className="text-sm font-semibold">Job Seekers</p>
+                                                    </div>
+
+                                                 
+
+
+
+                                                    <div className=" flex justify-between items-center  relative   before:absolute before:bottom-[-10px] before:w-full before:h-[1px] before:bg-[#ECECEC]">
+                                                        <div className="flex flex-col space-y-3">
+                                                        <p className="text-xs text-[#717171]">Email</p>
+                                                        <p className="text-sm font-semibold">xyzcompany@gmail.com</p>
+                                                        </div>
+                                                        
+
+                                                        <p className="p-2 text-[#104B53] text-xs rounded-full border border-[#104B53]">Change email</p>
+                                                    </div>
+
+
+
+
+                                                    <div className=" flex justify-between items-center  relative   before:absolute before:bottom-[-10px] before:w-full before:h-[1px] before:bg-[#ECECEC]">
+                                                        <div className="flex flex-col space-y-3">
+                                                        <p className="text-xs text-[#717171]">Password</p>
+                                                        <p className="text-sm font-semibold">*********</p>
+                                                        </div>
+                                                        
+
+                                                        <p className="p-2 text-[#104B53] text-xs rounded-full border border-[#104B53]">Change password</p>
+                                                    </div>
+
+
+
+
+
+                                                    <div className=" flex justify-between items-center  relative   before:absolute before:bottom-[-10px] before:w-full before:h-[1px] before:bg-[#ECECEC]">
+                                                        <div className="flex flex-col space-y-3">
+                                                        <p className="text-xs text-[#717171]">Phone</p>
+                                                        <p className="text-sm font-semibold">+91 949403539405</p>
+                                                        </div>
+                                                        
+
+                                                        <div className="flex flex-col space-y-3">   
+                                                        <p className="p-2 text-[#104B53] text-xs rounded-full border border-[#104B53]">Verify Phone number</p>
+
+                                                        <p className="p-2 text-[#104B53] text-xs rounded-full border border-[#104B53]">Change phone number</p>
+
+                                                        </div>
+                                                   
+                                                    </div>
+
+
+                                                    <div className=" flex justify-between items-center  relative   before:absolute before:bottom-[-10px] before:w-full before:h-[1px] before:bg-[#ECECEC]">
+                                                        <div className="flex flex-col space-y-3">
+                                                        <p className="text-xs text-[#717171]">xyzcompany@gmail.com</p>
+                                   
+                                                        </div>
+                                                        
+
+                                                        <p className="p-3 text-[#104B53] text-xs rounded-full bg-[#E9F358] ">Sign out</p>
+                                                    </div>
+
+
+
+                                                    <div className=" flex justify-between items-center  relative   before:absolute before:bottom-[-10px] before:w-full before:h-[1px] before:bg-[#ECECEC]">
+                                                        <p className="text-red-500 p-5">Close your account</p>
+                                                    </div>
+
+
+
+                                        </div>
+
+                        </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default AccountSetting;
