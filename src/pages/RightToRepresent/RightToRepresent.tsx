616c400fec7c4a5c7cff5ad05c2643535c3fdba8
Sandeep <sandbro7163337@gmail.com>
2024-09-02 14:04:25 +0530

    feat(RTR): add preivew page for RTR

--- src/pages/RightToRepresent/RightToRepresent.tsx
+++ src/pages/RightToRepresent/RightToRepresent.tsx
@@ -8,6 +8,7 @@ import Logo from '../../assets/logo/hirehour.png';
 const RightToRepresent: React.FC = () => {
   const [isRTROpen, setIsRTROpen] = useState<boolean>(false);
   const [isSignRTR, setIsSignRTR] = useState<boolean>(false);
+  const [isPreviewRTR,isSetPreviewRTR]=  useState<boolean>(false);
 
   const titles = [
     {
@@ -416,15 +417,173 @@ const RightToRepresent: React.FC = () => {
           <div className="p-4">
             <div className="w-full flex justify-center md:justify-end md:mt-4">
               <Link
-                to={'/additional-information'}
+                to={''}
+                onClick={()=>isSetPreviewRTR(true)}
                 className="flex justify-center items-center w-full md:w-36 h-10  rounded-full cursor-pointer bg-[#E9F358] "
               >
-                Continue
+                Preview
               </Link>
             </div>
           </div>
         </div>
       </div>
+
+
+         {/* View Preview RTR  */}
+
+
+
+      <div
+        className={`w-full h-full  flex justify-center items-center   fixed inset-0 transition-all ease-in-out duration-300  ${isPreviewRTR ? 'opacity-1 scale-[1.01]' : 'opacity-0 z-[-10]'} `}
+      >
+              <div className='w-full h-full absolute opacity-[.7] after:absolute after:left-0 after:w-full after:h-full after:bg-black '></div>
+       
+        <div
+          className="   z-[10] w-full max-w-[1000px] h-full shadow-xl
+                         overflow-auto  bg-[#F2F2F5] rounded-lg"
+        >
+          <div className="flex justify-between items-start p-10">
+            <div className="w-full flex justify-center items-center">
+              <img src={Logo} className="w-20 h-20" alt="img-logo" />
+            </div>
+            <IoMdClose size={40} color="#585858" onClick={()=>setIsSignRTR(false)} />
+          </div>
+
+          <div className="w-full max-w-[975px]  h-full m-auto border border-[#E1E1E2] rounded-lg">
+            <div className="w-full bg-white flex  flex-col justify-end items-end space-y-3 md:space-y-0 md:flex-row md:justify-between md:items-center p-5   rounded-t-lg">
+              <div className="flex items-center space-x-4">
+                <span className="text-lg text-[#6B7588]">Job Title: </span>
+                <p className="text-lg font-semibold">Full Stack Java Developer</p>
+                <span className="text-xs text-[#7B8496]">- Allen, Texas, US</span>
+              </div>
+
+              <div>
+                <p>Rate : $60 / HR</p>
+              </div>
+            </div>
+
+            <div className="w-full bg-white mt-3 flex flex-col space-y-10 md:space-y-0 md:flex-row md:justify-between md:items-start p-5">
+              <div className="flex flex-col space-y-3">
+                <h1 className="text-lg">From</h1>
+                <p>
+                  <strong>Send by</strong>: Erika Less
+                </p>
+                <p>
+                  <strong>Sender email</strong>: Erika@insightGlobal.com
+                </p>
+                <p>
+                  <strong>Company</strong>:Insight Global
+                </p>
+              </div>
+
+              <div className="flex flex-col items-start space-y-3">
+                <h1 className="text-lg">To</h1>
+                <p>
+                  <strong>Employer Name</strong>: Mathew
+                </p>
+                <p>
+                  <strong>Employer Email</strong>: Mathew@xyz.com
+                </p>
+                <p>
+                  <strong>Employer Company</strong>: AA Tech
+                </p>
+                <p>
+                  <strong>Applicant Name</strong>: Johnson
+                </p>
+                <p>
+                  <strong>Applicant Email</strong>: Johnson@zys.com
+                </p>
+              </div>
+
+              <div className="flex flex-col space-y-3 ">
+                <div className="flex  items-center">
+                  <MdOutlineFileDownload size={20} color="#104B53" />
+                  <p className="text-[#104B53] font-[600]">Export</p>
+                </div>
+              </div>
+            </div>
+
+            <hr />
+
+            <div className="w-full flex flex-col space-y-5 bg-white md:flex-row  items-start md:justify-between md:items-end p-5">
+            <div className="flex flex-col space-y-3">
+                  <p>
+                    <strong>Client</strong>: AT & T
+                  </p>
+                  <p>
+                    <strong>Prime Vendor </strong>: Insight Global
+                  </p>
+
+                  <p>
+                    <strong>Implementation</strong>: TCS
+                  </p>
+                  <p>
+                    <strong>Vendor </strong>: AA Tech
+                  </p>
+                </div>
+
+              <p>
+                <strong>Valid Till</strong>:30 days (08/26/2024)
+              </p>
+            </div>
+
+
+          {/* Acceptance  */}
+
+            <div className="w-full flex flex-col space-y-4 p-5 bg-white mt-2">
+              <p>
+                I, give the exclusive permission to (Candidates name) (Job Title) (Recruitment
+                agency) resume and qualification to position located in (Location) to represent
+                (Client) for the
+              </p>
+
+              <p>
+                I confirm that neither I have submitted my resume or application for this specific
+                position to any other recruitment agency within the period of the last 30 days, nor
+                I submitted Right to Represent form with any other recruitment agency for this job
+                requisition.
+              </p>
+
+              <p>
+                As a candidate, my handwritten signature, date and requisition number acknowledges
+                my authorization for the above-listed vendor to represent me for this posted
+                requisition from the date on the form till 30 days.
+              </p>
+
+              <p>
+                {' '}
+                <strong>Note:</strong> If RTR is submitted to more than one staffing vendor for the
+                same job requisition, then you will be pulled out of the consideration. You might be
+                no longer eligible for this requisition.
+              </p>
+
+              <div className='flex justify-start items-center space-x-3'>
+                <input type="checkbox"  className='w-[32px] h-[32px]'/> <p className='text-[#7D8697]'>By accepting this and proceeding further you agreed to the terms &  condition</p>
+              </div>
+            </div>
+
+             {/* Button submit  */}
+
+          <div className="p-4">
+            <div className="w-full flex justify-center md:justify-end md:mt-4">
+              <Link
+                to={''}
+                onClick={()=>isSetPreviewRTR(false)}
+                className="flex justify-center items-center w-full md:w-36 h-10  rounded-full cursor-pointer bg-[#E9F358] "
+              >
+                Add
+              </Link>
+            </div>
+          </div>
+          
+
+          </div>
+        </div>
+      </div>
+
+
+
+
 
       {/* View Sign RTR  */}
 
