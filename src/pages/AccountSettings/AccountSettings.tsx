1d323eb0bb8adac2ea9c95c0f526aa649aee64e9
Sandeep <sandbro7163337@gmail.com>
2024-09-04 18:23:15 +0530

    fix(accountSetting): fix mobile view

--- src/pages/AccountSettings/AccountSettings.tsx
+++ src/pages/AccountSettings/AccountSettings.tsx
@@ -23,11 +23,11 @@ const AccountSetting:React.FC=()=>{
             <div className=" w-full max-w-[1280px] h-full bg-white  m-auto mt-10 rounded-lg p-5">
                     <h1 className="text-3xl p-3 font-bold">Account Settings</h1>
                     <hr />
-                        <div className="w-full">
-                                <ul className="w-full  flex justify-start items-center space-x-6   ">
+                        <div className="w-full overflow-auto">
+                                <ul className="w-screen  md:w-auto flex justify-start items-center space-x-6   ">
                                     {titles?.map((item,id)=>{
                                         return(
-                                            <Link to={item.link} className={` text-sm p-1 md:text-xl font-[600]  ${id==0?'border-[#104B53] border-b-4':''}  md:p-5`} key={id}>{item.label}</Link>
+                                            <Link to={item.link} className={` text-xs p-1 md:text-lg font-[500]  ${id==0?'border-[#104B53] border-b-4':''}  md:p-5`} key={id}>{item.label}</Link>
                                         )
                                     })}
                                 </ul>
@@ -53,64 +53,64 @@ const AccountSetting:React.FC=()=>{
 
 
 
-                                                    <div className=" flex justify-between items-center  relative   before:absolute before:bottom-[-10px] before:w-full before:h-[1px] before:bg-[#ECECEC]">
-                                                        <div className="flex flex-col space-y-3">
+                                                    <div className=" flex flex-col space-y-3 md:space-y-3 md:flex-row md:justify-between items-center  relative   before:absolute before:bottom-[-10px] before:w-full before:h-[1px] before:bg-[#ECECEC]">
+                                                        <div className="flex flex-col w-full md:w-auto space-y-3">
                                                         <p className="text-xs text-[#717171]">Email</p>
                                                         <p className="text-sm font-semibold">xyzcompany@gmail.com</p>
                                                         </div>
                                                         
 
-                                                        <p  className="p-2 text-[#104B53] text-xs rounded-full border border-[#104B53]">Change email</p>
+                                                        <p  className="p-2 w-full md:w-auto text-[#104B53] text-xs text-center rounded-full border border-[#104B53]">Change email</p>
                                                     </div>
 
 
 
 
-                                                    <div className=" flex justify-between items-center  relative   before:absolute before:bottom-[-10px] before:w-full before:h-[1px] before:bg-[#ECECEC]">
-                                                        <div className="flex flex-col space-y-3">
+                                                    <div className=" flex flex-col space-y-3 md:space-y-3 md:flex-row md:justify-between items-center  relative   before:absolute before:bottom-[-10px] before:w-full before:h-[1px] before:bg-[#ECECEC]">
+                                                        <div className="flex flex-col w-full md:w-auto space-y-3">
                                                         <p className="text-xs text-[#717171]">Password</p>
                                                         <p className="text-sm font-semibold">*********</p>
                                                         </div>
                                                         
 
-                                                        <p className="p-2 text-[#104B53] text-xs rounded-full border border-[#104B53]" onClick={()=>setIsChangeEmail(true)}>Change password</p>
+                                                        <p className="p-2 w-full md:w-auto text-[#104B53] text-xs text-center rounded-full border border-[#104B53]" onClick={()=>setIsChangeEmail(true)}>Change password</p>
                                                     </div>
 
 
 
 
 
-                                                    <div className=" flex justify-between items-center  relative   before:absolute before:bottom-[-10px] before:w-full before:h-[1px] before:bg-[#ECECEC]">
-                                                        <div className="flex flex-col space-y-3">
+                                                    <div className=" flex flex-col space-y-3 md:space-y-3 md:flex-row md:justify-between items-center  relative   before:absolute before:bottom-[-10px] before:w-full before:h-[1px] before:bg-[#ECECEC]">
+                                                        <div className="flex flex-col w-full md:w-auto space-y-3">
                                                         <p className="text-xs text-[#717171]">Phone</p>
                                                         <p className="text-sm font-semibold">+91 949403539405</p>
                                                         </div>
                                                         
 
-                                                        <div className="flex flex-col space-y-3">   
-                                                        <p className="p-2 text-[#104B53] text-xs rounded-full border border-[#104B53]">Verify Phone number</p>
+                                                        <div className="flex flex-col space-y-3 w-full md:w-auto">   
+                                                        <p className="p-2 w-full md:w-auto text-[#104B53] text-xs text-center rounded-full border border-[#104B53]">Verify Phone number</p>
 
-                                                        <p className="p-2 text-[#104B53] text-xs rounded-full border border-[#104B53]">Change phone number</p>
+                                                        <p className="p-2 w-full md:w-auto text-[#104B53] text-xs text-center rounded-full border border-[#104B53]">Change phone number</p>
 
                                                         </div>
                                                    
                                                     </div>
 
 
-                                                    <div className=" flex justify-between items-center  relative   before:absolute before:bottom-[-10px] before:w-full before:h-[1px] before:bg-[#ECECEC]">
+                                                    <div className=" flex flex-col space-y-3 md:space-y-3 md:flex-row md:justify-between items-center  relative   before:absolute before:bottom-[-10px] before:w-full before:h-[1px] before:bg-[#ECECEC]">
                                                         <div className="flex flex-col space-y-3">
                                                         <p className="text-xs text-[#717171]">xyzcompany@gmail.com</p>
                                    
                                                         </div>
                                                         
 
-                                                        <p className="p-3 text-[#104B53] text-xs rounded-full bg-[#E9F358] ">Sign out</p>
+                                                        <p className="p-2 w-full md:w-auto text-[#104B53] text-xs text-center rounded-full border bg-[#E9F358] ">Sign out</p>
                                                     </div>
 
 
 
                                                     <div className=" flex justify-between items-center  relative   before:absolute before:bottom-[-10px] before:w-full before:h-[1px] before:bg-[#ECECEC]">
-                                                        <p className="text-red-500 p-5">Close your account</p>
+                                                        <p className="text-red-500 p-5 text-center w-full">Close your account</p>
                                                     </div>
 
 
@@ -121,10 +121,9 @@ const AccountSetting:React.FC=()=>{
             </div>
 
 
-<div className={`w-full h-full absolute top-0 transition-all ease-in-out duration-300  ${isChangeEmail ? 'opacity-1 scale-[1.01]' : 'opacity-0 z-[-10]'}`}>
-        <div className='w-full h-full absolute bg-black opacity-80 z-10'>
+<div className={`w-full h-full flex p-3   justify-center items-center  fixed inset-0 transition-all duration-500  ${isChangeEmail ? 'opacity-1 scale-[1.01]' : 'opacity-0 z-[-10]'}`}>
+<div className='w-full h-screen absolute opacity-[.7] after:absolute after:left-0 after:w-full after:h-full after:bg-black '></div>
 
-        </div>
        <div className='w-full h-full flex justify-center items-center'>
       
            <div className='relative z-20 w-[617px]   bg-white rounded-lg p-10 flex flex-col gap-3'>
