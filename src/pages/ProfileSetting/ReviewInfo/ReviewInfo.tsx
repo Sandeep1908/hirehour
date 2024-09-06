a1c2f4e3c449db9a52dd89f165fe470fdff936e6
Sandeep <sandbro7163337@gmail.com>
2024-09-06 14:17:48 +0530

    fix(compoenet): fix sizing issue in reiview and upload resume section

--- src/pages/ProfileSetting/ReviewInfo/ReviewInfo.tsx
+++ src/pages/ProfileSetting/ReviewInfo/ReviewInfo.tsx
@@ -29,7 +29,7 @@ const ReviewInfo: React.FC = () => {
           <hr />
 
           <div className="w-full p-4">
-            <ol className="flex items-center justify-between w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base">
+            <ol className="flex items-center justify-between w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-sm">
               <li className="flex md:w-full items-center text-[#104B53]   sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
                 <span className="flex items-center   sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
                   <span className="me-2 p-2 w-10 flex justify-center items-center md:w-14 h-10 bg-[#104B53] text-white rounded-full">
@@ -57,7 +57,7 @@ const ReviewInfo: React.FC = () => {
         </div>
       </div>
 
-      <div className="max-w-[1280px] h-full rounded-lg   bg-white m-auto p-4 mt-10">
+      <div className="max-w-[1080px] h-full rounded-lg   bg-white m-auto p-4 mt-10">
         <div className="w-full max-w-[1064px] m-auto pt-10 flex flex-col space-y-5">
           {/* Detal of Candidate  */}
 
@@ -65,16 +65,16 @@ const ReviewInfo: React.FC = () => {
             <h1 className="text-lg font-semibold">Detail of Candidate</h1>
             <div className="border p-7 border-[#EBEBF0] rounded-lg">
               <div className="flex justify-end items-center space-x-2">
-                <FaEdit color="#104B53" />
-                <p className="text-[#104B53]">Edit</p>
+                <FaEdit size={14} color="#104B53" />
+                <p className="text-[#104B53] text-xs" >Edit</p>
               </div>
 
               <div className="grid md:grid-cols-3 gap-5">
                 {details?.map((item, id) => {
                   return (
                     <div className="flex flex-col space-y-2" key={id}>
-                      <p className="text-[#838383]">{item.label}</p>
-                      <p className="font-semibold">{item.value}</p>
+                      <p className="text-[#838383] text-sm">{item.label}</p>
+                      <p className="font-semibold text-xs">{item.value}</p>
                     </div>
                   );
                 })}
@@ -86,12 +86,12 @@ const ReviewInfo: React.FC = () => {
 
           <div className=" flex flex-col space-y-3">
             <h1 className="text-lg font-semibold">Summery</h1>
-            <div className="border p-7 border-[#EBEBF0] rounded-lg">
+            <div className="border p-5 border-[#EBEBF0] rounded-lg">
               <div className="flex justify-end items-center space-x-2">
-                <FaEdit color="#104B53" />
-                <p className="text-[#104B53]">Edit</p>
+                <FaEdit size={14} color="#104B53" />
+                <p className="text-[#104B53] text-xs" >Edit</p>
               </div>
-              <p className="text-[#535354] text-base">
+              <p className="text-[#535354] text-justify text-sm">
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
                 Ipsum has been the industry's standard dummy text ever since the 1500s, when an
                 unknown printer took a galley of type and scrambled it to make a type specimen book.
@@ -113,36 +113,36 @@ const ReviewInfo: React.FC = () => {
               </h1>
 
               <div className="flex justify-end items-center space-x-2">
-                <BiPlus color="#104B53" />
-                <p className="text-[#104B53] font-semibold">Add</p>
+                <BiPlus size={14} color="#104B53" />
+                <p className="text-[#104B53] text-xs font-semibold">Add</p>
               </div>
             </div>
 
             <div className="border p-7 border-[#EBEBF0] rounded-lg">
               <div className="flex justify-end items-center space-x-4">
                 <div className="flex items-center  ">
-                  <FaEdit color="#104B53" />
-                  <p className="text-[#104B53]">Edit</p>
+                  <FaEdit size={14} color="#104B53" />
+                  <p className="text-[#104B53] text-xs" >Edit</p>
                 </div>
 
                 <div className="flex items-center ">
                   <MdDeleteOutline color="#104B53" />
-                  <p className="text-[#104B53]">Delete</p>
+                  <p className="text-[#104B53] text-xs">Delete</p>
                 </div>
               </div>
 
               <div className="flex flex-col space-y-5 ">
                 <div className="flex flex-col space-y-4">
-                  <h1 className="text-lg font-semibold">Java Fullstack</h1>
-                  <p className="text-[#6B7588]">xyz Company - Texas, United States</p>
+                  <h1 className="text-sm font-semibold">Java Fullstack</h1>
+                  <p className="text-[#6B7588] text-xs">xyz Company - Texas, United States</p>
                   <div className="flex space-x-3 flex-wrap  items-center w-full">
-                    <p className="flex justify-center items-center text-[#7C8596] bg-[#F2F2F5] p-[6px] md:p-3 rounded-full font-semibold text-xs">
+                    <p className="flex justify-center items-center text-[#7C8596] bg-[#F2F2F5] p-[6px] md:p-2 rounded-full font-semibold text-xs">
                       Health Care
                     </p>
-                    <p className="flex justify-center items-center text-[#7C8596] bg-[#F2F2F5] p-[6px] md:p-3 rounded-full font-semibold text-xs">
+                    <p className="flex justify-center items-center text-[#7C8596] bg-[#F2F2F5] p-[6px] md:p-2 rounded-full font-semibold text-xs">
                       Full Time
                     </p>
-                    <p className="flex justify-center items-center text-[#7C8596] bg-[#F2F2F5] p-[6px] md:p-3 rounded-full font-semibold text-xs">
+                    <p className="flex justify-center items-center text-[#7C8596] bg-[#F2F2F5] p-[6px] md:p-2 rounded-full font-semibold text-xs">
                       12/2022 - Present
                     </p>
                   </div>
@@ -152,11 +152,11 @@ const ReviewInfo: React.FC = () => {
 
                 <div className='flex flex-col space-y-4 '>
 
-                    <h1 className='text-lg '>Job summery</h1>
+                    <h1 className='text-sm '>Job summery</h1>
                     <ul className='flex flex-col space-y-3'>
-                        <li className='text-[#3A3A3C]'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </li>
+                        <li className='text-[#3A3A3C] text-xs'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </li>
 
-                        <li className='text-[#3A3A3C]'>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</li>
+                        <li className='text-[#3A3A3C] text-xs'>It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</li>
                     </ul>
                 </div>
 
@@ -180,30 +180,30 @@ const ReviewInfo: React.FC = () => {
               </h1>
 
               <div className="flex justify-end items-center space-x-2">
-                <BiPlus color="#104B53" />
-                <p className="text-[#104B53] font-semibold">Add</p>
+                <BiPlus size={14} color="#104B53" />
+                <p className="text-[#104B53] text-xs font-semibold">Add</p>
               </div>
             </div>
 
             <div className="border p-7 border-[#EBEBF0] rounded-lg">
               <div className="flex justify-end items-center space-x-4">
                 <div className="flex items-center  ">
-                  <FaEdit color="#104B53" />
-                  <p className="text-[#104B53]">Edit</p>
+                  <FaEdit size={14} color="#104B53" />
+                  <p className="text-[#104B53] text-xs" >Edit</p>
                 </div>
 
                 <div className="flex items-center ">
-                  <MdDeleteOutline color="#104B53" />
-                  <p className="text-[#104B53]">Delete</p>
+                  <MdDeleteOutline color="#104B53" size={14} />
+                  <p className="text-[#104B53] text-xs">Delete</p>
                 </div>
               </div>
 
               <div className="flex flex-col">
                 <div className="flex flex-col space-y-4">
-                  <h1 className="text-lg font-semibold">BE Computer Science</h1>
-                  <p className="text-[#6B7588]">xyz Collage- Texas, United State</p>
+                  <h1 className="text-sm font-semibold">BE Computer Science</h1>
+                  <p className="text-[#6B7588] text-xs">xyz Collage- Texas, United State</p>
                   <div className="flex space-x-3 items-center">
-                    <p className="text-[#7C8596] bg-[#F2F2F5] p-3 rounded-full font-semibold text-xs">
+                    <p className="text-[#7C8596] bg-[#F2F2F5] p-2 rounded-full font-semibold text-xs">
                     07/2016 - 06/2020
                     </p>
                      
@@ -225,17 +225,17 @@ const ReviewInfo: React.FC = () => {
               </h1>
 
               <div className="flex justify-end items-center space-x-2">
-                <BiPlus color="#104B53" />
-                <p className="text-[#104B53] font-semibold">Add</p>
+                <BiPlus size={14} color="#104B53" />
+                <p className="text-[#104B53] text-xs font-semibold">Add</p>
               </div>
             </div>
 
             <div className="border p-7 border-[#EBEBF0] rounded-lg">
               
 
-              <div className='w-full max-w-[765px] h-[100px] m-auto flex justify-center items-center border border-dashed border-[#C7C9D9] bg-[#F2F2F5] rounded-lg
+              <div className='w-full max-w-[765px] h-[80px] m-auto flex justify-center items-center border border-dashed border-[#C7C9D9] bg-[#F2F2F5] rounded-lg
               '>
-                    <p>Added certificate will be shown here</p>
+                    <p className='text-xs'>Added certificate will be shown here</p>
               </div>
             </div>
           </div>
@@ -244,8 +244,8 @@ const ReviewInfo: React.FC = () => {
  
   {/* more sections  */}
           <div className="flex space-x-2 justify-start items-center">
-                <FaPlus color="#104B53" size={20} />
-                <p className="text-[#104B53] text-lg font-semibold cursor-pointer">
+                <FaPlus color="#104B53" size={14} />
+                <p className="text-[#104B53] text-sm font-semibold cursor-pointer">
                   Add More Section
                 </p>
           </div>
@@ -255,10 +255,10 @@ const ReviewInfo: React.FC = () => {
           {/* buttion  */}
             
             <div className='flex flex-col space-y-5 md:flex-row justify-between items-center pb-10'>
-                <p className='text-[#7D8697]'>you can edit or modify the profile later in (profile)</p>
+                <p className='text-[#7D8697] text-xs w-full'>you can edit or modify the profile later in (profile)</p>
                 <div className="flex w-full  justify-between md:justify-end items-center  space-x-4">
-            <Link to={'/additional-information'} className="flex justify-center items-center w-full md:w-36 h-10  rounded-full cursor-pointer border border-[#104B53] text-[#104B53]">Back</Link>
-            <Link  to={''} className="flex justify-center items-center w-full md:w-36 h-10  rounded-full cursor-pointer bg-[#E9F358] ">
+            <Link to={'/additional-information'} className="flex justify-center items-center w-full md:w-36 h-8  rounded-full cursor-pointer border border-[#104B53] text-[#104B53]">Back</Link>
+            <Link  to={''} className="flex justify-center items-center w-full md:w-36 h-8  rounded-full cursor-pointer bg-[#E9F358] ">
               Submit
             </Link>
           </div>
--- src/pages/ProfileSetting/UploadResume/UploadResume.tsx
+++ src/pages/ProfileSetting/UploadResume/UploadResume.tsx
@@ -8,21 +8,21 @@ const UploadResume: React.FC = () => {
   const [isContinue, setIsContinue] = useState<boolean>(false);
   return (
     <div className="w-full h-full pb-20 bg-[#F6F6F8]">
-      <div className="max-w-[1280px] h-48  m-auto">
+      <div className="max-w-[1400px] h-48  m-auto">
         <div className="">
           <div className="flex  flex-col  space-y-4 md:flex-row  md:justify-between md:items-center p-4">
-            <h1 className="text-xl font-semibold">Setting up your profile</h1>
+            <h1 className="text-lg font-semibold">Setting up your profile</h1>
 
             <div className="flex justify-between md:justify-center items-center space-x-10">
-              <p className="font-semibold text-[#104B53]">Cancel </p>
-              <p className=" w-32 h-10 text-xs bg-[#104B53] md:bg-transparent  text-white md:w-40 md:h-10 rounded-full md:text-[#104B53]  flex justify-center items-center border border-[#104B53]">
+              <p className="font-semibold text-sm text-[#104B53]">Cancel </p>
+              <p className=" w-32 h-8 text-xs bg-[#104B53] md:bg-transparent  text-white   rounded-full md:text-[#104B53]  flex justify-center items-center border border-[#104B53]">
                 Save & Exit
               </p>
             </div>
           </div>
           <hr />
 
-          <div className="w-full p-4">
+          <div className="w-full p-1">
             <ol className="flex items-center justify-between w-full text-sm font-medium text-center text-gray-500 dark:text-gray-400 sm:text-base">
               <li className="flex md:w-full items-center text-[#104B53]   sm:after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-200 after:border-1 after:hidden sm:after:inline-block after:mx-6 xl:after:mx-10 dark:after:border-gray-700">
                 <span className="flex items-center   sm:after:hidden after:mx-2 after:text-gray-200 dark:after:text-gray-500">
@@ -49,18 +49,18 @@ const UploadResume: React.FC = () => {
 
       {/* Upload Resume  */}
 
-      <div className="max-w-[1280px] h-full md:h-[476px] bg-white m-auto p-4 mt-10">
-        <div className="flex flex-col space-y-4">
-          <h1 className="text-xl">Upload Your Resume</h1>
-          <p className="text-sm text-[#6B7588]">
+      <div className="max-w-[1280px] h-full md:h-[476px] bg-white m-auto p-4 mt-2">
+        <div className="flex flex-col space-y-2">
+          <h1 className="text-lg">Upload Your Resume</h1>
+          <p className="text-xs text-[#6B7588]">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry.
           </p>
         </div>
 
-        <div className="flex flex-col md:flex-row justify-center items-center p-4 md:p-10 space-x-5">
+        <div className="flex flex-col md:flex-row justify-center items-center p-4 md:p-5 space-x-5">
           <div className="w-full sm:w-[588px] h-[300px] border border-black border-dashed flex flex-col justify-center items-center">
-            <div className="flex flex-col space-y-5 justify-center items-center">
-              <p className="font-semibold"> Drap & Drop files Here</p>
+            <div className="flex flex-col space-y-3 justify-center items-center">
+              <p className="font-semibold text-sm"> Drap & Drop files Here</p>
               <p className="text-xs text-[#6B7588]">or</p>
 
               <label
@@ -72,7 +72,7 @@ const UploadResume: React.FC = () => {
               </label>
 
               <p className="text-xs text-[#6B7588]">
-                <span className="font-semibold text-sm text-black mr-4">Note:</span>
+                <span className="font-semibold text-sm text-black mr-2">Note:</span>
                 Supported File Type Pdf & Docx
               </p>
             </div>
@@ -82,15 +82,15 @@ const UploadResume: React.FC = () => {
 
           <div className="w-full sm:w-[588px] h-[300px] p-4 flex flex-col justify-around md:justify-between items-center">
             <div className="flex flex-col space-y-5 w-full">
-              <h1 className="text-lg font-semibold">Uploading</h1>
+              <h1 className="text-sm font-semibold">Uploading</h1>
 
               <div className="w-full flex justify-center items-center space-x-5">
-                <GrDocumentPdf size={50} />
+                <GrDocumentPdf size={30} />
 
                 <div className="flex flex-col space-y-3 w-full">
-                  <p>resume.pdf 7.8mb</p>
+                  <p className='text-xs'>resume.pdf 7.8mb</p>
 
-                  <div className="w-full bg-[#FFF1C6] rounded-full  ">
+                  <div className="w-full max-w-60 bg-[#FFF1C6] rounded-full  ">
                     <div
                       className="bg-[#FFD05B] text-xs  text-black text-center  leading-none rounded-full"
                       style={{ width: '75%' }}
@@ -107,10 +107,10 @@ const UploadResume: React.FC = () => {
               <hr />
 
               <div
-                className="w-full flex justify-center md:justify-end md:mt-4 "
+                className="w-full flex justify-center md:justify-end md:mt-2 "
                 onClick={() => setIsContinue(true)}
               >
-                <p className="flex justify-center items-center w-full md:w-36 h-10  rounded-full cursor-pointer bg-[#E9F358] ">
+                <p className="flex justify-center items-center w-full md:w-36 h-8 text-sm  rounded-full cursor-pointer bg-[#E9F358] ">
                   Continue
                 </p>
               </div>
@@ -124,16 +124,16 @@ const UploadResume: React.FC = () => {
         className={`w-full h-full flex   justify-center items-center  fixed inset-0 transition-all duration-500 ${isContinue ? 'opacity-1 scale-[1.01]' : 'opacity-0 z-[-10]'} `}
       >
         <div
-          className="   z-[10] w-full max-w-[1000px] shadow-xl
-                        h-screen md:h-[667px] bg-white rounded-lg"
+          className="   z-[10] w-full max-w-[600px] shadow-xl
+                        h-[80%] bg-white rounded-lg"
         >
           <div className="flex justify-between items-center p-5">
             <div className="flex flex-col space-y-3">
-              <h1 className="text-xl font-semibold">Verify your contact information</h1>
+              <h1 className="text-lg font-semibold">Verify your contact information</h1>
               <p className="text-sm text-[#6B7588]">Verify your contact information</p>
             </div>
 
-            <IoMdClose size={30} onClick={() => setIsContinue(false)} className="cursor-pointer" />
+            <IoMdClose size={20} onClick={() => setIsContinue(false)} className="cursor-pointer" />
           </div>
 
           <hr />
@@ -143,14 +143,14 @@ const UploadResume: React.FC = () => {
               {/* first name  */}
               <div className="flex flex-col space-y-2">
                 <div className="flex ">
-                  <label htmlFor="">First Name</label>
+                   <label htmlFor="" className='text-sm' >First Name</label>
                   <span className="text-red-500">*</span>
                 </div>
 
                 <input
                   type="text"
                   placeholder="Enter your first name"
-                  className="p-4 border border-[#EBEBF0] rounded-md"
+                  className="p-4 border border-[#EBEBF0] rounded-md placeholder:text-xs"
                 />
               </div>
 
@@ -158,14 +158,14 @@ const UploadResume: React.FC = () => {
 
               <div className="flex flex-col space-y-2">
                 <div className="flex ">
-                  <label htmlFor="">Last Name</label>
+                   <label htmlFor="" className='text-sm'>Last Name</label>
                   <span className="text-red-500">*</span>
                 </div>
 
                 <input
                   type="text"
                   placeholder="Enter your last name"
-                  className="p-4 border border-[#EBEBF0] rounded-md"
+                  className="p-4 border border-[#EBEBF0] rounded-md placeholder:text-xs"
                 />
               </div>
             </div>
@@ -174,14 +174,14 @@ const UploadResume: React.FC = () => {
               {/* Phone No  */}
               <div className="flex flex-col space-y-2">
                 <div className="flex ">
-                  <label htmlFor="">Phone Number</label>
+                   <label htmlFor="" className='text-sm'>Phone Number</label>
                   <span className="text-red-500">*</span>
                 </div>
 
                 <input
                   type="text"
                   placeholder="Enter your phone no"
-                  className="p-4 border border-[#EBEBF0] rounded-md"
+                  className="p-4 border border-[#EBEBF0] rounded-md placeholder:text-xs"
                 />
               </div>
 
@@ -189,14 +189,14 @@ const UploadResume: React.FC = () => {
 
               <div className="flex flex-col space-y-2">
                 <div className="flex ">
-                  <label htmlFor="">Email</label>
+                   <label htmlFor="" className='text-sm'>Email</label>
                   <span className="text-red-500">*</span>
                 </div>
 
                 <input
                   type="email"
                   placeholder="Enter your email"
-                  className="p-4 border border-[#EBEBF0] rounded-md"
+                  className="p-4 border border-[#EBEBF0] rounded-md placeholder:text-xs"
                 />
               </div>
             </div>
@@ -204,11 +204,11 @@ const UploadResume: React.FC = () => {
             {/* sponsership  */}
             <div className="flex flex-col space-y-2">
               <div className="flex ">
-                <label htmlFor="">Visa Sponsorship</label>
+                 <label htmlFor="" className='text-sm'>Visa Sponsorship</label>
                 <span className="text-red-500">*</span>
               </div>
 
-              <select className="p-4 border border-[#EBEBF0] rounded-md">
+              <select className="p-4 border border-[#EBEBF0] rounded-md placeholder:text-xs">
                 <option>Required</option>
                 <option>Not Required</option>
               </select>
@@ -218,18 +218,18 @@ const UploadResume: React.FC = () => {
 
             <div className="flex flex-col space-y-2">
               <div className="flex ">
-                <label htmlFor="">Location</label>
+                 <label htmlFor="" className='text-sm'>Location</label>
                 <span className="text-red-500">*</span>
               </div>
 
               <input
                 type="email"
                 placeholder="Enter your email"
-                className="p-4 border border-[#EBEBF0] rounded-md"
+                className="p-4 border border-[#EBEBF0] rounded-md placeholder:text-xs"
               />
             </div>
 
-            <p className="text-[#6B7588]">
+            <p className="text-[#6B7588] text-xs">
               Recruiters may search by location for candidates to hire better
             </p>
           </div>
@@ -238,7 +238,7 @@ const UploadResume: React.FC = () => {
 
           <div className="p-4">
             <div className="w-full flex justify-center md:justify-end md:mt-4">
-              <Link to={'/additional-information'} className="flex justify-center items-center w-full md:w-36 h-10  rounded-full cursor-pointer bg-[#E9F358] ">
+              <Link to={'/additional-information'} className="flex justify-center items-center w-full md:w-36 h-8 text-xs  rounded-full cursor-pointer bg-[#E9F358] ">
                 Continue
               </Link>
             </div>
