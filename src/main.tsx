75449851ffbe5c72b551ae416f175a0a70318102
Sandeep <sandbro7163337@gmail.com>
2024-09-12 16:29:45 +0530

    feat(main): add routes for job seekers pages

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
- 
+
 import {
   RouterProvider,
   createBrowserRouter,
@@ -10,12 +10,12 @@ import {
   Route,
 } from 'react-router-dom';
 import Home from './pages/job-seekers/home/Home.tsx';
- 
+
 import Signup from './pages/auth/Signup/Signup.tsx';
 import Signin from './pages/auth/Signin/Signin.tsx';
- 
+
 import JobDescription from './pages/job-seekers/JobDescription/JobDescription.tsx';
- 
+
 import AdditionalInfo from './pages/job-seekers/ProfileSetting/AdditionalInformation/AdditionalInfo.tsx';
 import UploadResume from './pages/job-seekers/ProfileSetting/UploadResume/UploadResume.tsx';
 import ReviewInfo from './pages/job-seekers/ProfileSetting/ReviewInfo/ReviewInfo.tsx';
@@ -29,13 +29,15 @@ import JobPreference from './pages/job-seekers/Job Preference/JobPreference.tsx'
 import Accepted from './pages/job-seekers/RightToRepresent/Accepted/Accepted.tsx';
 import Decline from './pages/job-seekers/RightToRepresent/Decline/Decline.tsx';
 import Expired from './pages/job-seekers/RightToRepresent/Expired/Expired.tsx';
+import JobPosterHome from './pages/job-posters/home/Home.tsx';
+import JobBoard from './pages/job-posters/job-board/JobBasis.tsx';
+import JobDescriptionBoard from './pages/job-posters/job-board/JobDescription/JobDescriptionBoard.tsx';
+import CompanyProfile from './pages/job-posters/job-board/CompanyProfile/CompanyProfile.tsx';
 
- 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route path="/" element={<Home />} />
-
 
       <Route path="/searchjob" element={<JobDescription />} />
 
@@ -45,39 +47,44 @@ const router = createBrowserRouter(
       <Route path="/review-form" element={<ReviewInfo />} />
 
       {/* Myjobs  */}
-       <Route path="/myjobs" element={<MyJobs/>}/> 
+      <Route path="/myjobs" element={<MyJobs />} />
 
       {/* Right To Represent  */}
-      <Route path="/right-to-represent" element={<RightToRepresent/>}/> 
-      <Route path="/rtr-accepted" element={<Accepted/>}/>
-      <Route path="/rtr-decline" element={<Decline/>}/>
+      <Route path="/right-to-represent" element={<RightToRepresent />} />
+      <Route path="/rtr-accepted" element={<Accepted />} />
+      <Route path="/rtr-decline" element={<Decline />} />
 
-      <Route path="/rtr-expired" element={<Expired/>}/>
+      <Route path="/rtr-expired" element={<Expired />} />
 
-
-
-    {/* Messages  */}
-      <Route path="/messages" element={<Message/>}/> 
+      {/* Messages  */}
+      <Route path="/messages" element={<Message />} />
 
       {/* Account Setting  */}
-      <Route path="/account" element={<AccountSetting/>}/> 
-      <Route path="/profile-visibility" element={<ProfileVisibility/>}/> 
+      <Route path="/account" element={<AccountSetting />} />
+      <Route path="/profile-visibility" element={<ProfileVisibility />} />
 
-    {/* Pofile Page  */}
+      {/* Pofile Page  */}
 
-    <Route path="/profile" element={<Profile/>}/> 
+      <Route path="/profile" element={<Profile />} />
 
-    {/* Job preference  */}
+      {/* Job preference  */}
 
-    <Route path="/job-preference" element={<JobPreference/>}/> 
-
-
-
-
-
+      <Route path="/job-preference" element={<JobPreference />} />
 
       <Route path="/signup" element={<Signup />} />
       <Route path="/signin" element={<Signin />} />
+
+
+
+      {/* job posters route  */}
+      <Route path="/job-poster" element={<JobPosterHome/>} />
+      <Route path="/job-poster/job-basis" element={<JobBoard/>} />
+      <Route path="/job-poster/job-description" element={<JobDescriptionBoard/>} />
+      <Route path="/job-poster/company-profile" element={<CompanyProfile/>} />
+
+
+
+
     </Route>,
   ),
 );
