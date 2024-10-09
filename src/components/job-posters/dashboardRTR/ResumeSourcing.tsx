b14db78487b0b1422f37d210e1546b2391a50c7c
Sandeep <sandbro7163337@gmail.com>
2024-10-09 17:21:11 +0530

    feat(dashboardrtr): add resume sourcing

---
+++ src/components/job-posters/dashboardRTR/ResumeSourcing.tsx
@@ -0,0 +1,26 @@
+import VerifyJobIco from '../../../assets/dashboardrtr/verifyjob.png'
+const ResumeSourcing: React.FC = () => {
+  return (
+    <div className="w-full h-full">
+      <div className="flex justify-between items-center p-2">
+        <h1 className="text-sm font-semibold">Resume Sourcing</h1>
+      </div>
+      <hr />
+
+
+      <div className="w-full h-[82vh]  flex justify-center items-center ">
+            <div className='flex flex-col justify-center items-center max-w-5xl space-y-5'>
+                <img src={VerifyJobIco} className='w-56 h-48' alt="verify-job" />
+                <h1 className='text-sm font-semibold text-center'>Access Limited: <br />
+                Please Complete Verification to Unlock Full Features</h1>
+
+                <p className='text-xs text-center'>You're currently viewing a limited version of TopEquator. To gain full access to all job postings, employer tools, and exclusive features, please verify your account.Take a moment to verify now and start enjoying the complete benefits of our platform.</p>
+
+                <p className='text-[10px] font-[500] w-48 flex justify-center items-center rounded-full  h-7 bg-[#E9F358] text-[#104B53]'>Subscribe for Resume Sourcing </p>
+            </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResumeSourcing;
