765e152d59e2c6b211e46d18061ebac4654cae06
Sandeep <sandbro7163337@gmail.com>
2024-10-29 17:48:27 +0530

    feat(app): add route for header rtr

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,17 @@ import { Outlet } from 'react-router-dom';
 import { useLocation } from 'react-router-dom';
 import HeaderJP from './components/HeaderJP';
 import HeaderAdmin from './components/HeaderAdmin';
+import HeaderRTR from './components/HeaderRTR';
 
 const App: React.FC = () => {
   const pathname = useLocation().pathname;
   const route = pathname.includes('/job-poster');
   const adminRoute = pathname.includes('/admin');
+  const headerRTR=pathname.includes('/dashboard-rtr')
 
   return (
     <div className="w-full  bg-[#F2F2F5]">
-      {adminRoute ? <HeaderAdmin /> : route ? <HeaderJP /> : <Header />}
+      {adminRoute ? <HeaderAdmin /> : headerRTR ? <HeaderRTR/>: route ? <HeaderJP/>:<Header/>}
 
       <Outlet />
     </div>
