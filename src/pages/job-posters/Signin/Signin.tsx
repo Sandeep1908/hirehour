59a12444b5c839c5674a516bf32b8a4e7aa53813
Aditya <aadimalviyaa@gmail.com>
2024-12-07 00:16:58 +0530

    feat(page): add signup and singin in recruiter and admin

--- src/pages/job-posters/Signin/Signin.tsx
+++ src/pages/job-posters/Signin/Signin.tsx
@@ -1,23 +1,118 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import google_logo from '../../../assets/Google.svg';
 import apple_logo from '../../../assets/apple.svg';
 
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useMutation } from '@tanstack/react-query';
+import axiosInstance from '../../../axios/axiosInstance';
+import { toast } from 'react-toastify';
+import { AxiosError } from 'axios';
+import { z } from 'zod';
+import { jwtDecode } from 'jwt-decode';
  
+
+
+interface UserCredentials {
+  email: string;
+  password: string;
+}
+
+// Zod schema for validation
+const signinSchema = z.object({
+  email: z.string().email({ message: 'Invalid email address' }),
+  password: z.string().min(8, { message: 'Password must be at least 8 characters' }),
+});
+
+
+// Define the shape of form errors
+interface FormErrors {
+  email?: { _errors: string[] };
+  password?: { _errors: string[] };
+}
+
+interface JwtPayload {
+  userId: number;
+  userEmail: string;
+  permissions: string[];
+  roles: string[];
+  iat: number;
+  exp: number;
+}
+
 
 const Signin: React.FC = () => {
   const [showPassword, setShowPassword] = useState<boolean>(false);
-  const [forgetPassword, setForgetPassword] = useState<boolean>(false);
- 
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [formErrors, setFormErrors] = useState<FormErrors>({});
+  const [validateErrors, setValidateErrors] = useState<boolean>(false);
 
+  const navigate = useNavigate();
+
+  // Check if user is already logged in
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      navigate('/'); // Redirect to home or dashboard if the user is logged in
+    }
+  }, [navigate]);
+
+
+  const mutation = useMutation({
+    mutationFn: async (userCredentials: UserCredentials) => {
+      const response = await axiosInstance.post("/api/recruiter/login", userCredentials);
+       
+      return response.data;
+    },
+
+    onSuccess: (data) => {
+      toast.success('Logged In Successfull');
+      localStorage.setItem('topequatorTokenRecruiter',data?.token)
+      const decodedToken = jwtDecode<JwtPayload>(data?.token);
+      console.log("decodedToken",decodedToken)
+      if (decodedToken && decodedToken.permissions) {
+        const permissions = decodedToken.permissions;
+
+        if (permissions.length === 1) {
+           navigate('/dashboard-rtr'); // Redirect to Dashboard
+        } else if (permissions.length > 1) {
+           navigate('/job-poster'); // Redirect to RTR-Dashboard
+        }
+     }
+      // const redirectTo = location.state?.from?.pathname || '/';
+      // navigate(redirectTo);
+
+    },
+    onError: (error) => {
+      const axiosError = error as AxiosError<{message:string}>;
+      toast.error(axiosError?.response?.data?.message)
+      localStorage.removeItem("topequatorTokenRecruiter");
+      setValidateErrors(true)
+      setEmail("")
+      setPassword("")
+      navigate("/job-poster/signin");
+    },
+  });
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // Validate form using Zod
+    const result = signinSchema.safeParse({ email, password });
+    if (!result.success) {
+      setFormErrors(result.error.format());
+    } else {
+      setFormErrors({});
+      mutation.mutate({ email, password });
+    }
+  };
 
   const togglePasswordVisibility = () => {
     setShowPassword(!showPassword);
   };
   return (
 
-    <div className=' bg-[#114B53]   w-full h-[92vh] pt-10 lg:pt-10'>
+    <div className=' bg-[#114B53]   w-full h-[93vh] pt-10 lg:pt-10'>
     <div className='w-full h-full px-5 lg:px-10 flex gap-20'>
 
         <div className='hidden md:flex w-[50%]'>
@@ -30,47 +125,52 @@ const Signin: React.FC = () => {
                <p className='text-black text-[20px] md:text-lg font-bold'>Welcome</p>
                <p className='text-black text-[14px] md:text-sm font-normal mt-1' >Log in to your existing account with email</p>
 
-              <form action="">
+              <form onSubmit={handleSubmit}>
                
-                
+              <div className='w-full mt-4 md:mt-2  relative'>
+                <label htmlFor="email" className='text-[14px]'>
+                  Email <span className='text-[#E71717]'>*</span>
+                </label>
+                <div className='w-full h-[40px] mt-1 '>
+                  <input
+                    type="email"
+                    className='border-2 px-2 border-[#E1E1E2] w-full h-full rounded-lg'
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                 {formErrors?.email && <p className="absolute  text-red-500 text-[10px] ">{formErrors.email._errors?.[0]}</p>}
+                 {validateErrors && <p className="absolute  text-red-500 text-[10px] ">Invalid Creadential</p>}
+                </div>
+              </div>
 
 
-                
-                  <div className='mt-4 md:mt-2'>
-                         <label htmlFor="firstName" className='text-[14px] '>
-                            Email <span className='text-[#E71717]'>*</span>
-                         </label>
-                         <div className='w-full h-[40px] mt-1'>
 
-
-                         <input type="email" className='border-2 border-[#E1E1E2] w-full h-full rounded-lg' />
-                         </div>
-                     </div>
-
-
-
-                     <div className='w-full flex gap-5 mt-2'>
-                     <div className=' w-full'>
-                         <label htmlFor="firstName" className='text-[14px] '>
-                         Password <span className='text-[#E71717]'>*</span>
-                         </label>
-                         <div className='relative w-full h-[40px] mt-1'>
-
-
-                         <input type="text" className='border-2 border-[#E1E1E2] w-full h-full rounded-lg' />
-                         <button
-                               type="button"
-                               className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-600"
-                               onClick={togglePasswordVisibility}
-                           >
-                               {showPassword ? <FaEyeSlash /> : <FaEye />}
-                           </button>
-                         </div>
-                     </div>
-                   
+              <div className='w-full flex gap-5 mt-4'>
+                <div className=' w-full relative'>
+                  <label htmlFor="password" className='text-[14px]'>
+                    Password <span className='text-[#E71717]'>*</span>
+                  </label>
+                  <div className='relative w-full h-[40px] mt-1'>
+                    <input
+                      type={showPassword ? "text" : "password"}
+                      className=' px-2 border-2 border-[#E1E1E2] w-full h-full rounded-lg'
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <button
+                      type="button"
+                      className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-600"
+                      onClick={togglePasswordVisibility}
+                    >
+                      {showPassword ? <FaEyeSlash /> : <FaEye />}
+                    </button>
                   </div>
-                {/* </div>
-                 </div> */}
+                  {formErrors?.password && <span className="absolute bottom-[-15px] text-red-500  text-[10px]">{formErrors.password._errors?.[0]}</span>}
+                  {validateErrors && <p className="absolute  text-red-500 text-[10px] ">Invalid Creadential</p>}
+
+                </div>
+              </div>
+             
 
 
                   <div className='flex justify-between items-end mt-8 md:mt-2'>
@@ -79,16 +179,16 @@ const Signin: React.FC = () => {
                       <p className='text-[14px] '>Remember me</p>
                   </div>
 
-                  <div onClick={()=>{setForgetPassword(!forgetPassword)}} className='font-medium text-[14px] md:text-base underline'>
+                  <div  className='font-medium text-[14px] md:text-base underline'>
 
                   Forgot Password?
                   </div>
                   </div>
 
 
-                  <Link to={"/job-poster"} className='w-full h-[40px] flex justify-center items-center bg-[#E9F358] rounded-3xl mt-4'>
-                     <p className='text-base font-semibold'>Log in</p>
-                  </Link>
+                  <button type="submit" className='w-full h-[40px] flex justify-center items-center bg-[#E9F358] rounded-3xl mt-4'>
+                <p className='text-base font-semibold'>Log in</p>
+              </button>
 
                   <div className="flex mt-2 items-center justify-center ">
 
