f20fff2c8d852efd31437d6f86eecd1f66dab3da
Sandeep <sandbro7163337@gmail.com>
2024-12-23 16:48:10 +0530

    fix issue with import

--- src/pages/job-posters/pricing/Pricing.tsx
+++ src/pages/job-posters/pricing/Pricing.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { FaCheckCircle } from 'react-icons/fa';
 
 import { useQuery } from '@tanstack/react-query';
